4b22730d9628883cc305709807a4df9211805734
Dylan Mack <Dylan@Dylans-MacBook-Pro-2.local>
2020-11-20 15:41:32 -0500

    Updated data and added full description to main

--- weatherTool_data.py
+++ weatherTool_data.py
@@ -6,7 +6,7 @@ import pytz
 
 #OPENWEATHER VARS
 key = "eac567b880fd463cdbc956e98d9dd1dd"
-zipcode = '10504'
+zipcode = '14850'
 country = 'us'
 latitude = '42.444471'
 longitude = '-76.491595'
@@ -18,51 +18,71 @@ url_onecall='https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lo
 #openweather data 
 data_onecall = (req.get(url_onecall)).json()  #as json->dictionary
 
+
+
 #current time
-current_date = datetime.date.today()
-current_numberday = current_date.strftime('%d')  #turning the datetime object into a string as day with the strftime method
-current_time_est = datetime.datetime.now()
+current_date = datetime.date.today()  # native current year-month-day 
+current_numberday = current_date.strftime('%d')  # native current day number 
+current_weekday = str(datetime.datetime.now().strftime('%A'))  # native current weekday 
+current_time_est = str(datetime.datetime.now().strftime('%X'))  # native current time in hours:minute:second
+
+
 
 #current data
-current_time_unix = data_onecall['current']['dt']
-current_time_utc = datetime.datetime.utcfromtimestamp(data_onecall['current']['dt'])  # changing unix time to UTC
-current_feelslike_f = int(((data_onecall['current']['feels_like'])-273.15)*(9/5)+32)
-current_precip = data_onecall['current']['weather'][0]['main']
-current_description = data_onecall['current']['weather'][0]['description']
+current_time_unix_utc_weather = data_onecall['current']['dt']  # current unix dt in utc from openweather 
+current_date_weather = datetime.datetime.fromtimestamp(data_onecall['current']['dt'])  # current utc date from openweather
+current_time_est_weather = datetime.datetime.fromtimestamp(data_onecall['current']['dt']).strftime('%X')  # current est time from openweather
+if current_time_est_weather != current_time_est:  # ensuring that the current openweather and native est time are equal
+	delay_seconds = abs(int(current_time_est_weather[-2:])-int(current_time_est[-2:]))
+	print("ERROR, openweather is not sourcing current time. Delay is "+str(delay_seconds)+" seconds. Check line 33 for more information")
+current_temp_f = int(((data_onecall['current']['temp'])-273.15)*(9/5)+32)  # current temperature 
+current_feelslike_f = int(((data_onecall['current']['feels_like'])-273.15)*(9/5)+32)  # current feels like temperature  
+current_precip = data_onecall['current']['weather'][0]['main']  # current precipitation
+current_description = data_onecall['current']['weather'][0]['description']  # current description 
 
-#daily today, always at 12PM EST
-daily_time_unix = data_onecall['daily'][0]['dt']
-daily_time_utc = datetime.datetime.utcfromtimestamp(data_onecall['daily'][0]['dt'])
-daily_temp_day_f = int(((data_onecall['daily'][0]['feels_like']['day'])-273.15)*(9/5)+32)
-daily_precip = data_onecall['daily'][0]['weather'][0]['main']
+
+
+#daily data, always at 11am EST
+daily_time_unix_utc_weather = data_onecall['daily'][0]['dt']  # daily (4pm utc) unix dt in utc from openweather
+daily_date_weather = datetime.datetime.fromtimestamp(data_onecall['daily'][0]['dt'])  # daily (4pm utc) date from openweather
+daily_time_est_weather = datetime.datetime.fromtimestamp(data_onecall['daily'][0]['dt']).strftime('%X')  # daily time (11am est) from openweather
+if daily_time_est_weather != "11:00:00":  # ensuring that openweather is taking daily time at 11am est
+	print("ERROR, openweather is not sourcing accurate time. Check line 43 for more information.")
+daily_temp_day_f = int(((data_onecall['daily'][0]['temp']['day'])-273.15)*(9/5)+32)  # daily (11am est) day temperature
+daily_feelslike_f = int(((data_onecall['daily'][0]['feels_like']['day'])-273.15)*(9/5)+32)  # daily (11am est) day feels like temperature  
+daily_precip = data_onecall['daily'][0]['weather'][0]['main']  # daily (11am est) precipitation 
+daily_description = data_onecall['daily'][0]['weather'][0]['description']  # daily (11am est) description 
 try:  #avoiding errors for rain/snow key 
-	daily_rainvol = data_onecall['daily'][0]['rain']
+	daily_rainvol = data_onecall['daily'][0]['rain']  # daily (11am est) rain volume
 except KeyError:
-	print('')
+	print('',end='')
 try:  #avoiding errors for rain/snow key 
-	daily_snowvol = data_onecall['daily'][0]['snow']
+	daily_snowvol = data_onecall['daily'][0]['snow']  # daily (11am est) snow volume
 except KeyError:
-	print('')
-if int(current_numberday) != int(daily_time_utc.strftime('%d')):  #checking that the request pull is giving today's daily weather
-	speech.speak(speech.mimir, "Weather request is not functioning properly. Check the weather tool file line 44 for more information.")
+	print('',end='')
+if int(current_numberday) != int(daily_date_weather.strftime('%d')):  #checking that the request pull is giving today's daily weather
+	speech.speak(speech.mimir, "Weather request is not functioning properly. Check line 53 for more information.")
+
 
 
 #daily tomorrow, always at 12PM EST
-dailytom_time_unix = data_onecall['daily'][1]['dt']
-dailytom_time_utc = datetime.datetime.utcfromtimestamp(data_onecall['daily'][1]['dt'])
-dailytom_temp_day_f = int(((data_onecall['daily'][1]['feels_like']['day'])-273.15)*(9/5)+32)
-dailytom_precip = data_onecall['daily'][1]['weather'][0]['main']
+dailytom_time_unix_utc_weather = data_onecall['daily'][1]['dt']  #tomorrow daily unix dt from openweather
+dailytom_date_weather = datetime.datetime.fromtimestamp(data_onecall['daily'][1]['dt'])  # tomorrow daily (4pm UTC) date from openweather
+dailytom_time_est_weather = datetime.datetime.fromtimestamp(data_onecall['daily'][1]['dt']).strftime('%X')  # tomorrow daily time (11am est) from openweather
+if dailytom_time_est_weather != "11:00:00":
+		print("ERROR, openweather is not sourcing accurate time. Check line 66 for more information.")
+dailytom_temp_day_f = int(((data_onecall['daily'][1]['temp']['day'])-273.15)*(9/5)+32)  # tomorrow daily (11am est) day temperature 
+dailytom_feelslike_f = int(((data_onecall['daily'][1]['feels_like']['day'])-273.15)*(9/5)+32)  # tomorrow daily (11am est) day feels like temperature
+dailytom_precip = data_onecall['daily'][1]['weather'][0]['main']  # tomorrow daily (11am est) precipitation 
 try: 
-	dailytom_rainvol = data_onecall['daily'][1]['rain']
+	dailytom_rainvol = data_onecall['daily'][1]['rain']  # tomorrow daily (11am est) rain volume 
 except KeyError:
-	print('')
+	print('',end='')
 try:
-	dailytom_snowvol = data_onecall['daily'][1]['snow']
+	dailytom_snowvol = data_onecall['daily'][1]['snow']  # tomorrow daily (11am est) snow volume
 except KeyError:
-	print('')
-if int(current_numberday)+1 != int(dailytom_time_utc.strftime('%d')):  #checking that the request pull is giving tomorrows's daily weather
+	print('',end='')
+if int((current_date + datetime.timedelta(days=1)).strftime('%d')) != int(dailytom_date_weather.strftime('%d')):  #checking that the request pull is giving tomorrows's daily weather
 	speech.speak(speech.mimir, "Weather request is not functioning properly. Check the weather tool file line 52 for more information.")
-
-
 
 
--- weatherTool_main.py
+++ weatherTool_main.py
@@ -8,34 +8,37 @@ import speechtool as speech
 
 
 #PERSONAL VARS
-weather_time_switch = 18  #prompt time that switches weather data from today to tomorrow in 24-hour time 
+weather_time_switch = 20  #prompt time that switches weather data from today to tomorrow in 24-hour time 
 jacket_weather = 55  #temperature in f that suggests a jacket
 snowgear_cap = 30  #the amount of snow in mm that requires you to bring snow gear
 
 
 
 # Tells user whether they need a jacket now (or tomorrow if it is after weather_time_switch)
+
+def fullDescription():
+	speech.speak(speech.mimir, data.location+" currently feels like "+str(data.current_feelslike_f)+"degrees and"+data.current_description)
+	#speech.speak(speech.mimir, )
+
+
+
+
 def wearjacket(): 
 	umbrella=0
 	jacket=0
 	snow=0
-	
 
-	if int(data.current_time_est.strftime('%H')) >= weather_time_switch:  
-		#bring jacket logic tomorrow
-		time = 'tomorrow'
-		temp_f = data.dailytom_temp_day_f
-		if data.dailytom_precip == "Snow" and int(data.dailytom_snowvol) > snowgear_cap:
+	if int(data.current_time_est[:2]) >= weather_time_switch:  # Setting jacket logic for tomorrow
+		date = 'tomorrow'
+		if data.dailytom_precip == "Snow" and (int(data.dailytom_snowvol) > snowgear_cap):
 			snow = 1
 		else: 
 			if data.dailytom_precip == "Rain":
 				umbrella=1
 			elif data.dailytom_temp_day_f < jacket_weather:
 				jacket=1		
-	elif int(data.current_time_est.strftime('%H')) <weather_time_switch:
-		#bring jacket logic today
-		time = 'today'
-		temp_f = data.daily_temp_day_f
+	elif int(data.current_time_est[:2]) < weather_time_switch:  # Setting jacket logic for today 
+		date = 'today'
 		if data.daily_precip == "Snow" and int(data.daily_snowvol) > snowgear_cap:
 			snow = 1
 		else: 
@@ -44,30 +47,25 @@ def wearjacket():
 			elif data.daily_temp_day_f < jacket_weather:
 				jacket = 1	
 
-	#Full desc
-
-	speech.speak(speech.mimir, data.location+" feels like "+str(temp_f)+" and"+data.current_description)
-	
-
-	#desc
 	if snow==1:
-		speech.speak(speech.mimir,"Bring snow gear "+time)
+		speech.speak(speech.mimir,"Bring snow gear "+date)
 	else:
 		if umbrella==1 and jacket==1:
-			speech.speak(speech.mimir,"Bring a jacket and umbrella "+time)
+			speech.speak(speech.mimir,"Bring a jacket and umbrella "+date)
 		elif umbrella==1 and jacket==0:
-			speech.speak(speech.mimir,"Bring an umbrella "+time)
+			speech.speak(speech.mimir,"Bring an umbrella "+date)
 		elif umbrella==0 and jacket==1:
-			speech.speak(speech.mimir,"Bring a jacket "+time)
+			speech.speak(speech.mimir,"Bring a jacket "+date)
 		else:
-			if time == 'today':
-				speech.speak(speech.mimir, time+" will be a nice "+ str(data.daily_temp_day_f) + " degrees.")
-			elif time == 'tomorrow':
-				speech.speak(speech.mimir, time+" will be a nice "+ str(data.dailytom_temp_day_f) + " degrees.")
+			if date == 'today':
+				speech.speak(speech.mimir, date+" is a nice "+ str(data.daily_temp_day_f) + " degrees. You don't need a jacket.")
+			elif date == 'tomorrow':
+				speech.speak(speech.mimir, date+" will be a nice "+ str(data.dailytom_temp_day_f) + " degrees. You don't need a jacket.")
 
 			
 
 wearjacket()
+fullDescription()
 
 			
 
