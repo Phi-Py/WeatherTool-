9116d069faaf90f4b9992cdc32796051ff33b97e
Dylan Mack <Dylan@Dylans-MacBook-Pro-2.local>
2020-11-26 15:39:11 -0500

    Added locations on boot, and a README, and doubled main functionality.

--- weatherTool_data.py
+++ weatherTool_data.py
@@ -31,7 +31,7 @@ class Location:
 
 
 
-	def __init__(self, zipcode, country, latitude, longitude, location):
+	def __init__(self, location, country, zipcode, latitude, longitude):
 		self._zipcode = zipcode
 		self._country = country
 		self._latitude = latitude
@@ -59,7 +59,7 @@ class Location:
 		current_time_est_weather = datetime.datetime.fromtimestamp(self._data_onecall['current']['dt']).strftime('%X')  # current est time from openweather
 		if current_time_est_weather != self._current_time_est:  # ensuring that the current openweather and native est time are equal 
 			delay_seconds = abs(int(current_time_est_weather[-2:])-int(self._current_time_est[-2:]))
-			print("ERROR, openweather is not sourcing current time. Delay is "+str(delay_seconds)+" seconds. Check line 33 for more information")
+			print("ERROR, openweather is not sourcing current time. Delay is "+str(delay_seconds)+" seconds. Check line 62 for more information")
 		current_temp_f = int(((self._data_onecall['current']['temp'])-273.15)*(9/5)+32)  # current temperature 
 		current_feelslike_f = int(((self._data_onecall['current']['feels_like'])-273.15)*(9/5)+32)  # current feels like temperature  
 		current_precip = self._data_onecall['current']['weather'][0]['main']  # current precipitation
@@ -73,7 +73,7 @@ class Location:
 		daily_date_weather = datetime.datetime.fromtimestamp(self._data_onecall['daily'][0]['dt'])  # daily (4pm utc) date from openweather
 		daily_time_est_weather = datetime.datetime.fromtimestamp(self._data_onecall['daily'][0]['dt']).strftime('%X')  # daily time (11am est) from openweather
 		if daily_time_est_weather != "11:00:00":  # ensuring that openweather is taking daily time at 11am est
-			print("ERROR, openweather is not sourcing accurate time. Check line 43 for more information.")
+			print("ERROR, openweather is not sourcing accurate time. Check line 76 for more information.")
 		daily_temp_day_f = int(((self._data_onecall['daily'][0]['temp']['day'])-273.15)*(9/5)+32)  # daily (11am est) day temperature
 		daily_feelslike_f = int(((self._data_onecall['daily'][0]['feels_like']['day'])-273.15)*(9/5)+32)  # daily (11am est) day feels like temperature  
 		daily_precip = self._data_onecall['daily'][0]['weather'][0]['main']  # daily (11am est) precipitation 
@@ -89,7 +89,7 @@ class Location:
 			daily_snowvol = None
 			print('',end='')
 		if int(self._current_numberday) != int(daily_date_weather.strftime('%d')):  #checking that the request pull is giving today's daily weather
-			print("Weather request is not functioning properly. Check line 53 for more information.")
+			print("Weather request is not functioning properly. Check line 92 for more information.")
 
 		return daily_feelslike_f, daily_precip, daily_description, daily_rainvol, daily_snowvol
 
@@ -99,7 +99,7 @@ class Location:
 		dailytom_date_weather = datetime.datetime.fromtimestamp(self._data_onecall['daily'][1]['dt'])  # tomorrow daily (4pm UTC) date from openweather
 		dailytom_time_est_weather = datetime.datetime.fromtimestamp(self._data_onecall['daily'][1]['dt']).strftime('%X')  # tomorrow daily time (11am est) from openweather
 		if dailytom_time_est_weather != "11:00:00":
-			print("ERROR, openweather is not sourcing accurate time. Check line 66 for more information.")
+			print("ERROR, openweather is not sourcing accurate time. Check line 102 for more information.")
 		dailytom_temp_day_f = int(((self._data_onecall['daily'][1]['temp']['day'])-273.15)*(9/5)+32)  # tomorrow daily (11am est) day temperature 
 		dailytom_feelslike_f = int(((self._data_onecall['daily'][1]['feels_like']['day'])-273.15)*(9/5)+32)  # tomorrow daily (11am est) day feels like temperature
 		dailytom_precip = self._data_onecall['daily'][1]['weather'][0]['main']  # tomorrow daily (11am est) precipitation
@@ -115,7 +115,7 @@ class Location:
 			dailytom_snowvol = None
 			print('',end='')
 		if int((self._current_date + datetime.timedelta(days=1)).strftime('%d')) != int(dailytom_date_weather.strftime('%d')):  #checking that the request pull is giving tomorrows's daily weather
-			print("Weather request is not functioning properly. Check the weather tool file line 52 for more information.")
+			print("Weather request is not functioning properly. Check the weather tool file line 118 for more information.")
 
 		return dailytom_feelslike_f, dailytom_precip, dailytom_description, dailytom_rainvol, dailytom_snowvol
 
